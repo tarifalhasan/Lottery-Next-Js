d16d5656dff0f0813e45d27a74baf09d54493ea6
tarifalhasan <tarifalhasan999@gmail.com>
2023-02-26 21:15:41 +0600

    raffle drew project

--- src/components/Section/RaffleDraw.jsx
+++ src/components/Section/RaffleDraw.jsx
@@ -1,4 +1,60 @@
+import { useState, useEffect } from 'react';
 const RaffleDraw = () => {
+  const days = Number(16);
+
+  const [endDate, setEndDate] = useState(new Date()); // Initial end date is the current date
+  const [timeLeft, setTimeLeft] = useState({}); // Object to hold the time left in days, hours, minutes, and seconds
+
+  useEffect(() => {
+    const newEndDate = new Date();
+    newEndDate.setDate(newEndDate.getDate() + days);
+    setEndDate(newEndDate);
+  }, [days]);
+
+  // Update the time left every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = endDate.getTime() - now;
+
+      if (distance <= 0) {
+        clearInterval(interval);
+        setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      } else {
+        setTimeLeft({
+          days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+          hours: Math.floor(
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          ),
+          minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+          seconds: Math.floor((distance % (1000 * 60)) / 1000),
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [endDate]);
+
+  let day = timeLeft.days;
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  let hour = timeLeft.hours;
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let minutes = timeLeft.minutes;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let seconds = timeLeft.seconds;
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
   return (
     <div className="w-full max-w-[1000px] mx-auto relative  ">
       <div className=" z-40 px-4 sm:px-10 bg-white shadow-lg lg:shadow-2xl rounded-xl py-8">
@@ -21,19 +77,19 @@ const RaffleDraw = () => {
           <div className="lg:col-span-2 mb-5 lg:mb-0">
             <div className="flex justify-center md:justify-start gap-x-3 sm:gap-x-10">
               <div>
-                <span className="timer-box">01</span>
+                <span className="timer-box">{day}</span>
                 <span className="timer-hour">Days</span>
               </div>
               <div>
-                <span className="timer-box">01</span>
+                <span className="timer-box">{hour}</span>
                 <span className="timer-hour">Hours</span>
               </div>
               <div>
-                <span className="timer-box">01</span>
+                <span className="timer-box">{minutes}</span>
                 <span className="timer-hour">Minutes</span>
               </div>
               <div>
-                <span className="timer-box">01</span>
+                <span className="timer-box">{seconds}</span>
                 <span className="timer-hour">Seconds</span>
               </div>
             </div>
